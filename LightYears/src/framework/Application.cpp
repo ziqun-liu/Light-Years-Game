bb3e8c16c3bd43f9432d7dfb4aa5d75e412934d4
ziqun-liu <ziqun.liu@yahoo.com>
2025-01-07 13:38:09 -0800

    77. Setup the Game Loop

--- LightYears/src/framework/Application.cpp
+++ LightYears/src/framework/Application.cpp
@@ -1,14 +1,21 @@
+#include<iostream>
 #include "framework/Application.h"
 
 namespace ly
 {
 	Application::Application()
-		: mWindow{ sf::VideoMode(1024, 1440), "Light Years" }
+		: mWindow{ sf::VideoMode(1024, 1440), "Light Years" },
+		mTargetFrameRate{60.f},
+		mTickClock{}
 	{
 		
 	}
+
 	void Application::Run()
 	{
+		mTickClock.restart();
+		float accumulatedTime = 0.f;
+		float targetDeltaTime = 1.f / mTargetFrameRate;
 		while (mWindow.isOpen())
 		{
 			sf::Event windowEvent;
@@ -19,6 +26,23 @@ namespace ly
 					mWindow.close();
 				}
 			}
+
+			accumulatedTime += mTickClock.restart().asSeconds();
+			while (accumulatedTime > targetDeltaTime)
+			{
+				accumulatedTime -= targetDeltaTime;
+				Tick(targetDeltaTime);
+				Render();
+			}
 		}
+	}
+
+	void Application::Tick(float deltaTime)
+	{
+		std::cout << "ticking at framerate " << 1.f / deltaTime << std::endl;
+	}
+
+	void Application::Render()
+	{
 	}
 }
